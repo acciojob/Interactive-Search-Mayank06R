6f42fbf354e90badf8b5aace478adc6381aee23c
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-22 20:02:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+// script.js
+function activateSearch() {
+    const searchDiv = document.querySelector('.search');
+    const inputElement = document.querySelector('.input');
+
+    searchDiv.classList.add('active');
+    inputElement.focus();
+}
+
